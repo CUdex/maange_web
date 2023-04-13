081299d10a281dfa50c41a41adfd36827cb51a6a
cudex <cuyu9779@gmail.com>
2023-04-13 10:25:23 +0900

    git init

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.10.11-slim
+
+COPY ./app /app
+COPY ./requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+# set working directory
+WORKDIR /app
+
+# expose port
+EXPOSE 80
+
+# run server
+CMD ["/usr/local/bin/uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+
---
+++ app/main.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import mysql.connector
+import tools
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+
+    db_info = tools.readAppInfo()
+    # MySQL 연결 설정
+    mydb = mysql.connector.connect(
+      host=db_info['db_host'],
+      user=db_info['db_user'],
+      password=db_info['db_passwd'],
+      database=db_info['db_database']
+    )
+
+    # 쿼리 실행
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM vm_list")
+
+    # 쿼리 결과 출력
+    result = "<table class='styled-table'>"
+    result += "<thead>"
+    result += "<tr>"
+    for x in mycursor.description:
+        result += "<th>" + str(x[0]) + "</th>"
+    result += "</tr>"
+    result += "</thead>"
+    result += "<tbody>"
+    for x in mycursor:
+        result += "<tr>"
+        for i, value in enumerate(x):
+            if i == 0:
+                result += "<td>" + str(value[:-4]) + "</td>"
+            else:
+                result += "<td>" + str(value) + "</td>"
+        result += "</tr>"
+    result += "</tbody>"
+    result += "</table>"
+
+    
+    # MySQL 연결 종료
+    mycursor.close()
+    mydb.close()
+
+    style = """
+        <style>
+            .styled-table {
+                border-collapse: collapse;
+                margin: 25px 0;
+                font-size: 0.9em;
+                font-family: sans-serif;
+                min-width: 400px;
+                box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+            }
+
+            .styled-table thead tr {
+                background-color: #009879;
+                color: #ffffff;
+                text-align: center;
+            }
+
+            .styled-table th,
+            .styled-table td {
+                padding: 12px 15px;
+            }
+
+            .styled-table tbody tr {
+                border-bottom: 1px solid #dddddd;
+            }
+
+            .styled-table tbody tr:nth-of-type(even) {
+                background-color: #f3f3f3;
+            }
+
+            .styled-table tbody tr:last-of-type {
+                border-bottom: 2px solid #009879;
+            }
+
+            .styled-table tbody td {
+                text-align: center;
+            }
+        </style>
+    """
+
+    return style + result
+
